3ce0d4cd644740c3f8587a90e87bca372b50fa23
fuyutarow <fuyutarow@gmail.com>
2021-04-25 08:05:11 +0900

    Add Formula/version-make

--- Formula/version-make.rb
+++ Formula/version-make.rb
@@ -1,21 +1,22 @@
-# require_relative './relatives/version-make'
+require_relative './relatives/version-make.linux'
+require_relative './relatives/version-make.mac'
 
 
 class VersionMake < Formula
-  # version ::VERSION
+  version ::mac_VERSION
 
   desc "CLI tool for managing SemVer"
   homepage "https://github.com/fuyutarow/version-make"
   head "https://github.com/fuyutarow/version-make.git"
 
   if OS.mac?
-    sha256 "dffb6ccb90395d2e9d90b3dc7f80736a30d6b5be42c99e99b6b3df87e965e30c"
+    sha256 ::mac_SHA
   elsif OS.linux?
-    sha256 "7f35438588860e8f372ce4b3dbed96abc848ec35cb4a7826df49f033d595277d"
+    sha256 ::linux_SHA
   end
 
   def install
-    bin.install Binary => 'shelp'
+    bin.install => 'version-make'
   end
 
   test do
