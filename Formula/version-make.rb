af650486712db4e61749487271ed06edc576e487
GitHub Action <action@github.com>
2021-04-25 04:32:11 +0000

    Update formula on version-make/

--- Formula/version-make.rb
+++ Formula/version-make.rb
@@ -1,6 +1,6 @@
-$version = "v0.202104.11"
-$sha_linux = "fc66d76a14838b008883afb211a763f4f686291e2043ac396203ad9b0133e0ca"
-$sha_mac = ""
+$version = "v0.202104.12"
+$sha_linux = ""
+$sha_mac = "f6c9dfbf815b871ea041132969efa57811b2042038ef5ba4bd11d8c2940dd2a7"
 $formula_name = "version-make"
 
 class VersionMake < Formula
