27a562d4306202f1c9fd82b67e54914383154eb8
fuyutarow <fuyutarow@gmail.com>
2021-04-25 10:35:55 +0900

    Update Formula/snippets-make

--- Formula/snippets-make.rb
+++ Formula/snippets-make.rb
@@ -1,11 +1,11 @@
+$version_mac = "v0.202104.7"
+$sha_mac = "adc34831f12b3ebbd1989ef27087c09fc96d816ac47b67968fd9396711ba5e58"
+$version_linux = "v0.202104.7"
+$sha_linux = "adc34831f12b3ebbd1989ef27087c09fc96d816ac47b67968fd9396711ba5e58"
 $formula_name = "snippets-make"
 
-require "./versions/#{$formula_name}/mac"
-require "./versions/#{$formula_name}/linux"
-
-
 class SnippetsMake < Formula
-  version ::VERSION_mac
+  version $version_mac
   desc "A CLI tool for managing code snippets"
   homepage "https://github.com/fuyutarow/#{$formula_name}"
   head "https://github.com/fuyutarow/snippets-make.git"
@@ -13,10 +13,10 @@ class SnippetsMake < Formula
 
   if OS.mac?
     url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{version}/#{$formula_name}-#{version}-x86_64-mac.zip"
-    sha256 ::SHA_mac
+    sha256 $sha_mac
   elsif OS.linux?
     url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{version}/#{$formula_name}-#{version}-x86_64-linux.zip"
-    sha256 ::SHA_linux
+    sha256 $sha_linux
   end
 
   def install
