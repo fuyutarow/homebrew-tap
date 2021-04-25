600465168fe14f34590616fc2f6a79ecf2f8c38d
GitHub Action <action@github.com>
2021-04-25 04:19:01 +0000

    Update formula on version-make/

--- Formula/version-make.rb
+++ Formula/version-make.rb
@@ -1,19 +1,19 @@
-$version_linux = "v0.202104.8"
-$sha_linux = "1d2dec10702f292318f54aa603c19ae6cb83d1e8d6b928589ad950b4e829472a"
+$version = "v0.202104.11"
+$sha_linux = "fc66d76a14838b008883afb211a763f4f686291e2043ac396203ad9b0133e0ca"
+$sha_mac = ""
 $formula_name = "version-make"
 
 class VersionMake < Formula
-  version $version_linux
+  version $version_mac
   desc "A CLI tool for versioning"
   homepage "https://github.com/fuyutarow/version-make"
   head "https://github.com/fuyutarow/version-make.git"
 
-
   if OS.mac?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{version}/#{$formula_name}-#{version}-x86_64-mac.zip"
+    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
     sha256 $sha_mac
   elsif OS.linux?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{version}/#{$formula_name}-#{version}-x86_64-linux.zip"
+    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
     sha256 $sha_linux
   end
 
---
+++ versions/version-make.rb
@@ -0,0 +1,3 @@
+$version = "v0.202104.11"
+$sha_linux = "fc66d76a14838b008883afb211a763f4f686291e2043ac396203ad9b0133e0ca"
+$sha_mac = ""
