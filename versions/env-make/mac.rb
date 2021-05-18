2c7b27b728d1fbd6a890a1a0a14b15ca5d1bec9e
GitHub Action <action@github.com>
2021-05-18 01:13:53 +0000

    Update sha on env-make/mac

--- versions/env-make/mac.rb
+++ versions/env-make/mac.rb
@@ -1,2 +1,2 @@
 $version_mac = "v0.202105.3"
-$sha_mac = "7faa2006eb6fb2d68bcf19699635803e22501abfd87d8045de46cc5db2a74a52"
+$sha_mac = "da1cc4e0f9886f7f2c1e7aa31cf477d5f7c379ab5867d84e9a5c9163c83f5b3b"
