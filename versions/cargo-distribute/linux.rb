f28f3f85a2e88999a5e37e9fa38572c78cd95359
GitHub Action <action@github.com>
2021-05-18 00:41:34 +0000

    Update sha on cargo-distribute/linux

--- versions/cargo-distribute/linux.rb
+++ versions/cargo-distribute/linux.rb
@@ -1,2 +1,2 @@
-$version_linux = "v0.202105.5"
-$sha_linux = "ab276c0cca030328efc79412065cd8da77660615cf7867835810224140a5d2ef"
+$version_linux = "v0.202105.6"
+$sha_linux = "a73939c964ebf4b030e2a78272b6b0a3cdd0e8fb39731055e0d95709e478c48f"
