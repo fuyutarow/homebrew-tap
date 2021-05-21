5bf5992b1e025ad18b9b273ffe157188d36cf275
fuyutarow <fuyutarow@gmail.com>
2021-05-22 08:16:05 +0900

    Add templates/partiql-cli

---
+++ templates/partiql-cli.rb
@@ -0,0 +1,24 @@
+$formula_name = "partiql-cli"
+
+class PartiqlCli < Formula
+  version $version_mac
+  desc ""
+  homepage ""
+  head ""
+
+  if OS.mac?
+    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
+    sha256 $sha_mac
+  elsif OS.linux?
+    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
+    sha256 $sha_linux
+  end
+
+  def install
+    bin.install "partiql-cli"
+  end
+
+  test do
+    system "#{bin}/partiql-cli", '--version'
+  end
+end
