8fc66f765256f149658b16176771067eaf557bc7
fuyutarow <fuyutarow@gmail.com>
2021-05-22 09:02:41 +0900

    Fix Formula/partiql-cli

--- Formula/partiql-cli.rb
+++ Formula/partiql-cli.rb
@@ -7,14 +7,14 @@ $formula_name = "partiql-cli"
 class PartiqlCli < Formula
   version $version_mac
   desc ""
-  homepage ""
-  head ""
+  homepage "https://github.com/fuyutarow/partiql-rs"
+  head "https://github.com/fuyutarow/partiql-rs.git"
 
   if OS.mac?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
+    url "https://github.com/fuyutarow/partiql-rs/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
     sha256 $sha_mac
   elsif OS.linux?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
+    url "https://github.com/fuyutarow/partiql-rs/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
     sha256 $sha_linux
   end
 
