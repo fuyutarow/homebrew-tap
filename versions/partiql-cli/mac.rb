f5aefb7d686a60c822eb5835ac68af0f1abd563d
GitHub Action <action@github.com>
2021-05-23 15:15:48 +0000

    Update sha on partiql-cli/mac

--- versions/partiql-cli/mac.rb
+++ versions/partiql-cli/mac.rb
@@ -1,2 +1,2 @@
-$version_mac = "v0.202105.5"
-$sha_mac = "eb6344a49de0ad0e00b198c2ef3ef7a770a8c54d788a49a1d7a2738bec213d29"
+$version_mac = "v0.202105.7"
+$sha_mac = "683ce76136cd14f7cd4aeca6f86c53cb31af79fc6703bb008c3397f8ba64ec19"
