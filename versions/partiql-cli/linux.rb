88f1ce081c8637133f44f7db0bf3c403f7b52ff9
GitHub Action <action@github.com>
2021-05-23 15:15:52 +0000

    Update sha on partiql-cli/linux

--- versions/partiql-cli/linux.rb
+++ versions/partiql-cli/linux.rb
@@ -1,2 +1,2 @@
-$version_linux = "v0.202105.5"
-$sha_linux = "574034d0ae5e91f8e497db5929d1d8dc86caf1f27576b52cf249c67fc8670068"
+$version_linux = "v0.202105.7"
+$sha_linux = "8741b57208dd9a45bc1f71ab8e65838ba15cdf7e6e774b8ef22c6aa0cd281077"
