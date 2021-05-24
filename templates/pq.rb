fd296f7d4df584ba9aae1da9b8dced8aacbdc3e2
fuyutarow <fuyutarow@gmail.com>
2021-05-25 06:56:09 +0900

    Add templates/pq

---
+++ templates/pq.rb
@@ -0,0 +1,24 @@
+$formula_name = "pq"
+
+class Pq < Formula
+  version $version_mac
+  desc "A implementation of PartiQL"
+  homepage "https://github.com/fuyutarow/partiql-rs"
+  head "https://github.com/fuyutarow/partiql-rs.git"
+
+  if OS.mac?
+    url "https://github.com/fuyutarow/partiql-rs/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
+    sha256 $sha_mac
+  elsif OS.linux?
+    url "https://github.com/fuyutarow/partiql-rs/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
+    sha256 $sha_linux
+  end
+
+  def install
+    bin.install "pq"
+  end
+
+  test do
+    system "#{bin}/pq", '--version'
+  end
+end
