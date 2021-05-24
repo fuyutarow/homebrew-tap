57c16cc25998cdc271147809fbd45786c7e8ae3b
GitHub Action <action@github.com>
2021-05-24 21:52:32 +0000

    Update sha on cargo-distribute/mac

--- versions/cargo-distribute/mac.rb
+++ versions/cargo-distribute/mac.rb
@@ -1,2 +1,2 @@
-$version_mac = "v0.202105.10"
-$sha_mac = "6c588f247734a071befd9bcff034098f6626f7c7c881e340383b01a98adb3177"
+$version_mac = "v0.202105.11"
+$sha_mac = "5e95ee4a99affee8867a81e79264a466f0b2c0a02bac2823d42ea269ee1a8bb4"
