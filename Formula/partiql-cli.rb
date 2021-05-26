4c1bf75c04ad7d260e7695e78a1993a3b4fae61e
GitHub Action <action@github.com>
2021-05-26 01:59:33 +0000

    Update formula on partiql-cli

--- Formula/partiql-cli.rb
+++ Formula/partiql-cli.rb
@@ -1,7 +1,7 @@
-$version_linux = "v0.202105.12"
-$sha_linux = "a797d0b47f6e1857642aa1d20ffdeb2bf486388482fb4c099dc3771bfb06d297"
-$version_mac = "v0.202105.12"
-$sha_mac = "7aba0a6f3542bd93b8d32cefa735a64d6647a5c703fa85b71cada86303c39706"
+$version_linux = "v0.202105.15"
+$sha_linux = "0a075f6873296b683eb6a149f4cca0eda867782640f192f5013a21bd4fd93076"
+$version_mac = "v0.202105.15"
+$sha_mac = "3b6a7ac46ec9e36a1751f599429664ca2fb31500b30469d84e2349a205e234ef"
 $formula_name = "partiql-cli"
 
 class PartiqlCli < Formula
