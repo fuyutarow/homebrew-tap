6baeadaf0e0507454b6486331aebcf251df6eba3
GitHub Action <action@github.com>
2021-06-02 09:27:39 +0000

    Update formula on tests-make

--- Formula/tests-make.rb
+++ Formula/tests-make.rb
@@ -1,7 +1,7 @@
-$version_linux = "v0.202106.3"
-$sha_linux = "d13ef6e431bf4900eab80eeff7033aee6b33ff8bd9bba7d188d083510e968b8c"
-$version_mac = "v0.202106.3"
-$sha_mac = "62ce206d6d8b62e8b68dbbecd7652e0234599eb3feb6a62bbf0488ef72ffb250"
+$version_linux = "v0.202106.4"
+$sha_linux = "b3b6f55e64be00a7ca0005b3d8c46e693047fcf756d8866ca57da05df88b059f"
+$version_mac = "v0.202106.4"
+$sha_mac = "f7a498b3d43eb2ce4f2d5903661145b89f5a5b7d2c18fd83faa97eb3e2e757de"
 $formula_name = "tests-make"
 
 class TestsMake < Formula
