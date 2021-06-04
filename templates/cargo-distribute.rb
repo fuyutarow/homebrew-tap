a792732575047eb9576b5211a5393d57e72b19fd
fuyutarow <fuyutarow@gmail.com>
2021-06-04 13:23:24 +0900

    Update templates/cargo-distribute

--- templates/cargo-distribute.rb
+++ templates/cargo-distribute.rb
@@ -2,15 +2,15 @@ $formula_name = "cargo-distribute"
 
 class CargoDistribute < Formula
   version $version_mac
-  desc "A CLI tool"
+  desc "Template generator for continuous distribution of rust projects to homebrew, linuxbrew and scoop"
   homepage "https://github.com/fuyutarow/cargo-distribute"
   head "https://github.com/fuyutarow/cargo-distribute.git"
 
   if OS.mac?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
+    url "https://github.com/fuyutarow/cargo-distribute/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
     sha256 $sha_mac
   elsif OS.linux?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
+    url "https://github.com/fuyutarow/cargo-distribute/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
     sha256 $sha_linux
   end
 
