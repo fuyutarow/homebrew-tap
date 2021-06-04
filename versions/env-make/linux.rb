5e9e8f674f7010ee29f7ce1af68041da9f3d9eb4
GitHub Action <action@github.com>
2021-06-04 08:42:54 +0000

    Update sha on env-make/linux

--- versions/env-make/linux.rb
+++ versions/env-make/linux.rb
@@ -1,2 +1,2 @@
 $version_linux = "v0.202106.1"
-$sha_linux = "46dcae2b7f28d319bee1222bf3ad90bc73ff7db8edbcc183f797a1d680d75137"
+$sha_linux = "1edd502b7ba7a0d44dedb0cf0eb54260151f4a3c171055aab09c93c0ead34b04"
