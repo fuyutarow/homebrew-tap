276c35cde32226a9d336838533508fd2157c546b
fuyutarow <fuyutarow@gmail.com>
2021-06-04 16:57:31 +0900

    Update templates/env-make

--- templates/env-make.rb
+++ templates/env-make.rb
@@ -2,15 +2,15 @@ $formula_name = "env-make"
 
 class EnvMake < Formula
   version $version_mac
-  desc ""
-  homepage ""
-  head ""
+  desc "A CLI tool for portable alias across shells and OS"
+  homepage "https://github.com/fuyutarow/env-make"
+  head "https://github.com/fuyutarow/env-make.git"
 
   if OS.mac?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
+    url "https://github.com/fuyutarow/env-make/releases/download/#{$version_mac}/#{$formula_name}-#{$version_mac}-x86_64-mac.zip"
     sha256 $sha_mac
   elsif OS.linux?
-    url "https://github.com/fuyutarow/#{$formula_name}/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
+    url "https://github.com/fuyutarow/env-make/releases/download/#{$version_linux}/#{$formula_name}-#{$version_linux}-x86_64-linux.zip"
     sha256 $sha_linux
   end
 
