410c818d58403bda0be3c4785add0398c25514e9
GitHub Action <action@github.com>
2021-06-07 12:37:17 +0000

    Update sha on pq/linux

--- versions/pq/linux.rb
+++ versions/pq/linux.rb
@@ -1,2 +1,2 @@
-$version_linux = "v0.202106.1"
-$sha_linux = "301c438d33857ce4dead079f5a019fbaa1e76f1f6d861b9e6052953e7301590e"
+$version_linux = "v0.202106.2"
+$sha_linux = "14221477527ce1ac21965f5fd18d26104ceecb7c8422d1439d97278f85fae2ad"
