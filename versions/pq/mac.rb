ad6c3c2506cfd1f64e455e61cee51daa9920bd52
GitHub Action <action@github.com>
2021-06-08 07:17:31 +0000

    Update sha on pq/mac

--- versions/pq/mac.rb
+++ versions/pq/mac.rb
@@ -1,2 +1,2 @@
-$version_mac = "v0.202106.2"
-$sha_mac = "1461edf84871d97a91bc9cf3000a619d6b0da46d1cf512d51ad3a29567dabb4b"
+$version_mac = "v0.202106.3"
+$sha_mac = "16f1f4369090f117863e8352c793747b203c86dbbbae2d50467614efd1f72849"
